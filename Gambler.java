b7b215d2ef7a41f208833f89d10cd636efc853f2
Thomas Schmidt <thomasschmidt@Thomas-Macbook.local>
2023-11-27 09:21:27 -0500

    Minor

--- Gambler.java
+++ Gambler.java
@@ -2,13 +2,10 @@ class Gambler {
     private Hand hand;
     private String name;
 
-    public Gambler() {
-
-    }
-
-    public void setName(String name) {
+    public Gambler(String name) {
         this.name = name;
     }
+
     public String getName() {
         return this.name;
     }
